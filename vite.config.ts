430140c7a1d65fb2159691f3b02f853406688309
YutLee <664009360@qq.com>
2022-02-21 12:53:26 +0800

    新增 demo 打包模式。

--- vite.config.ts
+++ vite.config.ts
@@ -3,30 +3,40 @@ import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    dts({
-      outputDir: 'lib'
-    })
-  ],
-  build: {
-    lib: {
-      entry: './src/index.ts',
-      name: 'cd-design-mobile',
-      // formats: ['es'],
-      fileName: (format) => `index.${format}.js`
-    },
-    rollupOptions: {
-      // 确保外部化处理那些你不想打包进库的依赖
-      external: ['react'],
-      output: {
-        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
-        globals: {
-          react: 'React'
-        }
+export default defineConfig(({ command, mode }) => {
+  if (mode !== 'demo') {
+    return {
+      plugins: [
+        react(),
+        dts({
+          outputDir: 'lib'
+        })
+      ],
+      build: {
+        lib: {
+          entry: './src/index.ts',
+          name: 'cd-design-mobile',
+          // formats: ['es'],
+          fileName: (format) => `index.${format}.js`
+        },
+        rollupOptions: {
+          // 确保外部化处理那些你不想打包进库的依赖
+          external: ['react'],
+          output: {
+            // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+            globals: {
+              react: 'React'
+            }
+          }
+        },
+        outDir: 'lib/src'
       }
-    },
-    outDir: 'lib/src'
+    }
+  } else {
+    return {
+      plugins: [
+        react()
+      ]
+    }
   }
 })
